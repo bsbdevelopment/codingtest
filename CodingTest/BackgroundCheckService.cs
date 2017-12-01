38a880bc7049713d54fa73307e7af1dd70e321d6
Mike Williams <mike.williams@bluesombrero.com>
2017-12-01 09:57:57 -0500

    Initial Commit

---
+++ CodingTest/BackgroundCheckService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodingTest
+{
+    public static class BackgroundCheckService
+    {
+        public static string RegisterApplicant(string firstName, string lastName, string dateOfBirth)
+        {
+            //Code to send application information to background check API.
+            return Guid.NewGuid().ToString();
+        }
+
+        public static bool VerifyApplication(string ssn, string applicantId)
+        {
+            //Code to send the SSN and Applicant ID to background check API
+            return ssn == "555-55-5555";
+        }
+    }
+}
---
+++ CodingTest/Controllers/VerifyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class VerifyController : Controller
+    {
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Post([FromBody]string firstName, [FromBody]string lastName, [FromBody]string dateOfBirth)
+        {
+            return Ok(BackgroundCheckService.RegisterApplicant(firstName, lastName, dateOfBirth));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]string ssn, [FromBody]string applicantId)
+        {
+            return Ok(BackgroundCheckService.VerifyApplication(ssn, applicantId));
+        }
+
+
+    }
+}
